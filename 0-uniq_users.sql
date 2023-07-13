26b4e7c7b6400fcd42b4d464d7711d4eb2875e2c
alex-chulu <chulualexandar@gmail.com>
2023-07-13 21:13:46 +0000

    We are all unique

---
+++ 0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- Create users table
+-- Description: This script creates a table called "users" with the specified attributes.
+-- Database: MySQL 5.7
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255)
+);
+
