349f042ec6a8ca26cc37a99da52c19d494393cd3
alex-chulu <chulualexandar@gmail.com>
2023-07-13 21:33:39 +0000

    best band ever

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,9 @@
+-- Rank country origins of bands by number of non-unique fans
+-- Description: This script ranks the country 
+-- origins of bands based on the number of non-unique fans.
+-- Database: Any
+
+SELECT origin, SUM(nb_fans) AS total_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY total_fans DESC;
