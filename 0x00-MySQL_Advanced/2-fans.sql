2b075430b02d6315bfdbe320ed80ebc1ab57d998
alex-chulu <chulualexandar@gmail.com>
2023-07-13 21:38:33 +0000

    task 2 fix

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -3,7 +3,5 @@
 -- origins of bands based on the number of non-unique fans.
 -- Database: Any
 
-SELECT origin, SUM(nb_fans) AS total_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY total_fans DESC;
+SELECT origin, SUM(nb_fans) AS total_fans FROM metal_bands
+GROUP BY origin ORDER BY total_fans DESC;
