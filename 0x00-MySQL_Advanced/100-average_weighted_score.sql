f513435f1dfe08a99929f5b3f6844aa3b2f35dc3
alex-chulu <chulualexandar@gmail.com>
2023-07-14 22:07:26 +0000

    task 12

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,22 +6,29 @@
 
 DELIMITER //
 
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser (
-  IN user_id INT
-)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
 BEGIN
   DECLARE average_weighted_score INT DEFAULT 0;
-  
-  -- Compute average weighted score
-  SELECT SUM(score * weight) / SUM(weight) INTO average_weighted_score
-  FROM corrections
-  WHERE user_id = user_id;
-  
-  -- Update user's average weighted score
-  UPDATE users
-  SET average_weighted_score = average_weighted_score
-  WHERE id = user_id;
+  DECLARE t_weight INT DEFAULT 0;
+
+  SELECT SUM(corrections.score * projects.weight) INTO t_weighted_score
+  FROM corrections 
+  INNER JOIN projects ON corrections.project_id = projects.id
+  WHERE corrections.user_id = user_id;
+
+  SELECT SUM(projects.weight) INTO tot_weight FROM corrections
+  INNER JOIN projects ON corrections.project_id = projects.id
+  WHERE corrections.user_id = user_id;
+
+  IF t_weight = 0 THEN
+    UPDATE users
+    SET users.average_score = 0
+    WHERE users.id = user_id;
+  ELSE
+    UPDATE users
+    SET users.average_score = tot_weighted_score / tot_weight
+    WHERE users.id = user_id;
+    END IF;
   
 END //
 
