9e7a71b3def76712b709b5dfed10d9031bc69c86
alex-chulu <chulualexandar@gmail.com>
2023-07-19 20:39:43 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,12 +2,12 @@
 
 """
 Module: cache
-Description: A simple Cache class to store data in Redis.
+Description: A simple Cache class to store data in Redis and retrieve with optional conversion function.
 """
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 class Cache:
     """
@@ -19,6 +19,9 @@ class Cache:
     Methods:
         __init__(): Initializes the Redis client and flushes the database.
         store(data: Union[str, bytes, int, float]) -> str: Stores the input data in Redis and returns the key.
+        get(key: str, fn: Callable = None) -> Union[str, bytes, int, float]: Retrieves the data from Redis and converts using the provided function.
+        get_str(key: str) -> str: Retrieves a UTF-8 encoded string from Redis.
+        get_int(key: str) -> int: Retrieves an integer from Redis.
     """
 
     def __init__(self) -> None:
@@ -42,18 +45,57 @@ class Cache:
         self._redis.set(key, data)
         return key
 
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """
+        Retrieves the data from Redis and converts it using the provided function (if any).
+
+        Args:
+            key (str): The key to retrieve data from Redis.
+            fn (Callable, optional): A callable function to convert the retrieved data (Default: None).
+
+        Returns:
+            Union[str, bytes, int, float]: The retrieved data (possibly converted using the provided function).
+        """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieves a UTF-8 encoded string from Redis.
+
+        Args:
+            key (str): The key to retrieve data from Redis.
+
+        Returns:
+            str: The retrieved UTF-8 encoded string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieves an integer from Redis.
+
+        Args:
+            key (str): The key to retrieve data from Redis.
+
+        Returns:
+            int: The retrieved integer.
+        """
+        return self.get(key, fn=int)
+
 # Example usage:
 if __name__ == "__main__":
     cache = Cache()
 
-    # Storing various types of data
-    key1 = cache.store("Hello, Redis!")
-    key2 = cache.store(42)
-    key3 = cache.store(b"Binary data")
-    key4 = cache.store(3.14)
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
 
-    print(f"Key 1: {key1}")
-    print(f"Key 2: {key2}")
-    print(f"Key 3: {key3}")
-    print(f"Key 4: {key4}")
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
 
