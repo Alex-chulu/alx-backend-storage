bfb3073154492bdd2e50df8a441d6341362f0ad4
alex-chulu <chulualexandar@gmail.com>
2023-10-08 22:06:54 +0000

    stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python3
-"""Script to provide stats about Nginx logs stored in MongoDB."""
-
-from typing import List
+'''Analyzes Nginx request logs stored in MongoDB and prints statistics.
+'''
 from pymongo import MongoClient
 
-def print_stats(collection):
-    """Print statistics about Nginx logs."""
-    total_logs = collection.count_documents({})
+def print_nginx_request_logs(nginx_collection):
+    '''Prints statistics about Nginx request logs.
+    '''
+    # Count the total number of logs
+    log_count = nginx_collection.count_documents({})
+    print(f'Total logs: {log_count}')
 
-    print(f"{total_logs} logs")
-
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("Methods:")
+    # Count logs for each HTTP method (GET, POST, PUT, PATCH, DELETE)
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print('HTTP Methods:')
     for method in methods:
-        method_count = collection.count_documents({"method": method})
-        print(f"    method {method}: {method_count}")
+        method_count = nginx_collection.count_documents({'method': method})
+        print(f'{method}: {method_count}')
 
-    status_check_count = collection.count_documents({
-        "method": "GET",
-        "path": "/status"
-    })
-    print(f"{status_check_count} status check")
+    # Count status check logs
+    status_check_count = nginx_collection.count_documents({'method': 'GET', 'path': '/status'})
+    print(f'Status check logs: {status_check_count}')
 
-if __name__ == "__main__":
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client.logs
-    collection = db.nginx
+def run():
+    '''Connects to MongoDB, retrieves Nginx logs, and prints statistics.
+    '''
+    # Connect to MongoDB
+    client = MongoClient('mongodb://127.0.0.1:27017')
 
-    print_stats(collection)
+    # Retrieve Nginx logs collection
+    nginx_collection = client.logs.nginx
+
+    # Analyze and print statistics for Nginx logs
+    print_nginx_request_logs(nginx_collection)
+
+if __name__ == '__main__':
+    run()
 
